97cfac0a53406936c07b3d3bfacb48db9646a901
anthonyeisenback <anthony.eisenback07@gmail.com>
2018-05-07 15:42:42 -0400

    Added Working Pages for Lot list, Schedules, and Registers tab for management purposes

--- app/src/main/java/com/example/anthonyeisenback/supervisy/MainActivity.java
+++ app/src/main/java/com/example/anthonyeisenback/supervisy/MainActivity.java
@@ -1,13 +1,96 @@
 package com.example.anthonyeisenback.supervisy;
-
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.anthonyeisenback.supervisy.Tabs.LotList;
+import com.example.anthonyeisenback.supervisy.Tabs.Registers;
+import com.example.anthonyeisenback.supervisy.Tabs.Schedules;
+
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Toolbar toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+
+        mViewPager =  findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_fragment_tabs, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    }
+
+class SectionsPagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                LotList tab1 = new LotList();
+                return tab1;
+            case 1:
+                Schedules tab2 = new Schedules();
+                return tab2;
+            case 2:
+                Registers tab3 = new Registers();
+                return tab3;
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
     }
 }
---
+++ app/src/main/java/com/example/anthonyeisenback/supervisy/Tabs/Registers.java
@@ -0,0 +1,31 @@
+package com.example.anthonyeisenback.supervisy.Tabs;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.anthonyeisenback.supervisy.R;
+
+public class Registers extends Fragment {
+
+    public static Registers newInstance() {
+
+        Bundle args = new Bundle();
+
+        Registers fragment = new Registers();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.registers_layout, container, false);
+        return view;
+    }
+}
